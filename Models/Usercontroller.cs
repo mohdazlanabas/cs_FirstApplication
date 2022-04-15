5621434b50fcb0380dda7c9470934f53f0941636
Azlan Abas <mohdazlan.abas@gmail.com>
2022-04-15 09:42:15 +0800

    First Update

---
+++ Models/Usercontroller.cs
@@ -0,0 +1,9 @@
+﻿namespace baseapp.Models
+{
+    public class Usercontroller
+    {
+        public int Mykad { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
